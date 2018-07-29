0bd0dc17d7e9e2199a1fd0c41c3e1834fc2eda5a
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-07-29 02:52:23 +0200

    Add upload image controller response

--- server/src/routes/post.routes.js
+++ server/src/routes/post.routes.js
@@ -1,9 +1,27 @@
 import { Router } from 'express';
 import * as PostController from '../controllers/post.controller';
 import { validator } from './validators/post.validator'
-import uploadImage from './middlewares/postImages'
+// import uploadImage from './middlewares/postImages'
 
 const router = new Router();
+
+import multer from 'multer'
+import mkdirp from 'mkdirp'
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		mkdirp(`public/images/${req.params.id}`, function (err) {
+			if (err) console.error(err)
+			cb(null, `public/images/${req.params.id}`)
+		});
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname)
+	}
+})
+
+const uploadImage = multer({ storage: storage })
+
 
 
 // Get all Posts
@@ -13,7 +31,7 @@ router.route('/').post(PostController.addPost);
 
 router.route('/:id').put(PostController.updatePost);
 
-router.route('/:id').post(uploadImage, PostController.uploadPostImage);
+router.route('/:id').post(uploadImage.single('image'), PostController.uploadPostImage);
 
 router.route('/:id').delete(PostController.deletePost);
 
