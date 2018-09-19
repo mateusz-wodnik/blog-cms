963e38b7357ca03f1ffc8314c88bb5244d3b5e43
mateusz-wodnik <mateusz.wodnik@gmail.com>
2018-09-19 02:09:22 +0200

    Add install npm script, Fix server gulpfile to v4 version of gulp

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -2,23 +2,25 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var nodemon = require('gulp-nodemon');
 
-gulp.task('default', ['server', 'nodemon'], function () {
-	gulp.watch('src/**/*.js', ['server'])
-})
-
-
 gulp.task('server', function() {
-	gulp.src('src/**/*.js')
-		.pipe(babel({
-			presets: ['env'],
-			plugins: ['transform-object-rest-spread']
-		}))
-		.pipe(gulp.dest('dist'))
+    return gulp.src('src/**/*.js')
+        .pipe(babel({
+            presets: ['env'],
+            plugins: ['transform-object-rest-spread']
+        }))
+        .pipe(gulp.dest('dist'))
 });
 
+
 gulp.task('nodemon', function () {
-	nodemon({
-		script: 'dist/server.js',
-		ext: 'js html'
-	})
-})
+    nodemon({
+        script: 'dist/server.js',
+        ext: 'js html',
+    });
+});
+
+gulp.task('default', gulp.series('server', 'nodemon', function () {
+	gulp.watch('src/**/*.js', ['server'])
+}));
+
+// var server = gulp.task('server');
